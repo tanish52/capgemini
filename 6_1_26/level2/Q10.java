21f7987e7f9dadb04de3992df4af3ad9284fdb8d
tanish52 <tanishsingla03@gmail.com>
2026-01-06 14:01:00 +0530

    changes

--- 6_1_26/level2/Q10.java
+++ 6_1_26/level2/Q10.java
@@ -1,7 +1,7 @@
 package level2;
 import java.util.Scanner;
 
-class q10 {
+class Q10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
--- 6_1_26/level2/Q9.java
+++ 6_1_26/level2/Q9.java
@@ -1,7 +1,7 @@
 package level2;
 import java.util.Scanner;
 
-class q9 {
+class Q9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
