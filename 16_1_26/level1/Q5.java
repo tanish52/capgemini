e9d3506af61445ef8f5d294e270440582a6f19e6
tanish52 <tanishsingla03@gmail.com>
2026-01-18 11:57:38 +0530

    constructor modified

--- 16_1_26/level1/Q5.java
+++ 16_1_26/level1/Q5.java
@@ -1,31 +1,31 @@
-import java.util.*;
-class Book1 {
-    String title;
-    String author;
-    int price;
-    boolean availability;
+    import java.util.*;
+    class Book1 {
+        String title;
+        String author;
+        int price;
+        boolean availability;
 
-    Book1(String title, String author, int price) {
-        this.title = title;
-        this.author = author;
-        this.price = price;
-        this.availability = true;
-    }
+        Book1(String title, String author, int price) {
+            this.title = title;
+            this.author = author;
+            this.price = price;
+            this.availability = true;
+        }
 
-    void borrowBook() {
-        if (availability) {
-            availability = false;
-            System.out.println("Book borrowed successfully: " + title);
-        } else {
-            System.out.println("Book is already borrowed.");
+        void borrowBook() {
+            if (availability) {
+                availability = false;
+                System.out.println("Book borrowed successfully: " + title);
+            } else {
+                System.out.println("Book is already borrowed.");
+            }
         }
     }
-}
 
-public class Q5 {
-    public static void main(String[] args) {
-        Book1 b1 = new Book1("Java Programming", "James Gosling", 500);
-        b1.borrowBook();
-        b1.borrowBook();
+    public class Q5 {
+        public static void main(String[] args) {
+            Book1 b1 = new Book1("Java Programming", "James Gosling", 500);
+            b1.borrowBook();
+            b1.borrowBook();
+        }
     }
-}
