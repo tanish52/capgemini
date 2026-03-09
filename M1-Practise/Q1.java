ec0a63a44446b40245c037fc9f24089337587d24
tanish52 <tanishsingla03@gmail.com>
2026-03-09 09:47:16 +0530

    m1 test

---
+++ M1-Practise/Q1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Q1{
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int [] arr=new int[n];
+    for(int i=0;i<arr.length;i++){
+      arr[i]=sc.nextInt();
+    }
+    int r=sc.nextInt();
+    int u=sc.nextInt();
+    int total=r*u;
+    int count=0;
+    int sum=0;
+    for(int i=0;i<arr.length;i++){
+      sum+=arr[i];
+      count++;
+      if(sum>=total){
+        System.out.println(count);
+        return;
+      }
+    }
+    System.out.println(0);
+  }
+}
